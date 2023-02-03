93895aa2bd1c47e86514f7054e46f1b7e1f0ac31
Susann Struwe <Susann.Struwe@gmail.com>
2023-02-03 17:32:04 +0100

    Added date-picker based on input type date

---
+++ src/app/datepickers/date-picker/date-picker.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+
+registerLocaleData(localeDe, 'de-DE', 'de');
+
+@Component({
+  selector: 'date-picker-component',
+  standalone: true,
+  imports:[CommonModule, FormsModule, FontAwesomeModule],
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.scss']
+})
+export class DatePickerComponent {
+
+  faCalender = faCalendar;
+
+  @Input() date?: Date;
+  @Output() dateChanged = new EventEmitter<Date>();
+
+  constructor() { }
+
+  changeDate(event: Event): void{
+    this.dateChanged.emit(this.date);
+  }
+
+  showDatePicker(){
+    const inputDateElement: HTMLInputElement | null = document.querySelector('#date-input');
+    if(inputDateElement){
+      inputDateElement.showPicker();
+    }
+  }
+}
