dff0b629bd194a08756104600c1e322f644ed928
Susann Struwe <Susann.Struwe@gmail.com>
2023-02-15 09:50:30 +0100

    Updated preview

--- src/app/layout/preview/preview.component.ts
+++ src/app/layout/preview/preview.component.ts
@@ -1,12 +1,13 @@
 
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from 'src/app/cards/card/card.component';
 
 
 @Component({
   selector: 'preview-component',
   standalone: true,
-  imports:[RouterModule],
+  imports:[RouterModule, CardComponent],
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
