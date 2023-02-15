932097b2a85a24ca788408feb29f437d5d011595
Susann Struwe <Susann.Struwe@gmail.com>
2023-02-15 14:09:03 +0100

    Updated input property

--- src/app/loaders/loader/loader.component.ts
+++ src/app/loaders/loader/loader.component.ts
@@ -14,7 +14,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 })
 export class LoaderComponent {
 
-  @Input() style?: string;
+  @Input() type?: string;
   //example -> linear, ease
   @Input() animation?: string;  
 
