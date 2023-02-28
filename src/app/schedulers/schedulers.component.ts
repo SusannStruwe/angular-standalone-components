e9ad715bd19167469844e8f38b277ed2e798fbec
Susann Struwe <Susann.Struwe@gmail.com>
2023-02-28 15:11:34 +0100

    Names of states generalized

--- src/app/schedulers/schedulers.component.ts
+++ src/app/schedulers/schedulers.component.ts
@@ -58,25 +58,25 @@ export class SchedulersComponent {
     const schedulerRow: SchedulerRow = new SchedulerRow(
       'Ressource 1', 
       null, 
-      [ new SchedulerEvent('1','12233', null, moment().subtract(3, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().set({ hours: 9, minutes: 0 }).toDate(), PlanningState.STARTED + (striped ? ' striped': '')),
-        new SchedulerEvent('2','44555', null, moment().add(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(2, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.MAINTAIN + (striped ? ' striped': ''))
+      [ new SchedulerEvent('1','12233', null, moment().subtract(3, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().set({ hours: 9, minutes: 0 }).toDate(), PlanningState.STATE1 + (striped ? ' striped': '')),
+        new SchedulerEvent('2','44555', null, moment().add(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(2, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.STATE3 + (striped ? ' striped': ''))
       ]);
     const schedulerRow2: SchedulerRow = new SchedulerRow(
       'Ressource 2', 
       null, 
-      [new SchedulerEvent('3','66666', null, moment().subtract(2, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().subtract(1, 'days').set({ hours: 11, minutes: 0 }).toDate(), PlanningState.DISRUPTED + (striped ? ' striped': '')), 
-      new SchedulerEvent('4', '4444',  null, moment().set({ hours: 9, minutes: 0 }).toDate(), moment().add(3, 'days').set({ hours: 16, minutes: 0 }).toDate(), PlanningState.STARTED + (striped ? ' striped': ''))
+      [new SchedulerEvent('3','66666', null, moment().subtract(2, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().subtract(1, 'days').set({ hours: 11, minutes: 0 }).toDate(), PlanningState.STATE4 + (striped ? ' striped': '')), 
+      new SchedulerEvent('4', '4444',  null, moment().set({ hours: 9, minutes: 0 }).toDate(), moment().add(3, 'days').set({ hours: 16, minutes: 0 }).toDate(), PlanningState.STATE1 + (striped ? ' striped': ''))
       ]);
     const schedulerRow3: SchedulerRow = new SchedulerRow(
       'Ressource 3', 
       null, 
-      [new SchedulerEvent('5','12233', null, moment().subtract(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().set({ hours: 8, minutes: 0 }).toDate(), PlanningState.STARTED + (striped ? ' striped': '')), 
-      new SchedulerEvent('6','44555', null, moment().add(2, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(5, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.MAINTAIN + (striped ? ' striped': ''))
+      [new SchedulerEvent('5','12233', null, moment().subtract(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().set({ hours: 8, minutes: 0 }).toDate(), PlanningState.STATE1 + (striped ? ' striped': '')), 
+      new SchedulerEvent('6','44555', null, moment().add(2, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(5, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.STATE3 + (striped ? ' striped': ''))
       ]);
     const schedulerRow4: SchedulerRow = new SchedulerRow(
       'Ressource 4',
        null, 
-       [new SchedulerEvent('7','44555', null,moment().subtract(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(1, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.STARTED + (striped ? ' striped': ''))
+       [new SchedulerEvent('7','44555', null,moment().subtract(1, 'days').set({ hours: 9, minutes: 0 }).toDate(), moment().add(1, 'days').set({ hours: 17, minutes: 0 }).toDate(), PlanningState.STATE1 + (striped ? ' striped': ''))
       ]);
     const schedulerRows: SchedulerRow[] = [schedulerRow, schedulerRow2, schedulerRow3, schedulerRow4];
 
